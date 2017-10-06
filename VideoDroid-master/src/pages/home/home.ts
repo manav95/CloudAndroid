be94b987f52d30923599bb844b231a818f90ead8
Mabb <mdutta7@gatech.edu>
2017-10-05 22:56:27 -0400

    Vid

---
+++ VideoDroid-master/src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureVideoOptions } from '@ionic-native/media-capture';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public mediaCapture: MediaCapture) {
+
+  }
+
+  startVideo(event) {
+     console.log(event)
+     let options: CaptureVideoOptions = { limit: 1 };
+     this.mediaCapture.captureVideo(options).then(
+         (data: MediaFile[]) => console.log(data),
+         (err: CaptureError) => console.error(err)
+     );
+
+  }
+
+}
